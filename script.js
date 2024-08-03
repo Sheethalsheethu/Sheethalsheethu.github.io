5575238a6bceb67a51b51b8c40ce56827612a8fe
Sheethalsheethu <gowdasheethal2@gmail.com>
2024-08-03 10:31:30 +0530

    updated

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.profile-pic').addEventListener('mouseover', function() {
-    this.src = 'profile-hover.jpg'; // Change to the hover image
-});
-
-document.querySelector('.profile-pic').addEventListener('mouseout', function() {
-    this.src = 'profile.jpg'; // Change back to the original image
-});
